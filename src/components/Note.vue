ecf374f285722c6e2e1513246e443f48a115db7c
lunaticaq <Lindasalan05@gmail.com>
2024-07-29 14:00:28 +0200

    altro css(date)

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -161,7 +161,7 @@ export default {
       const year = date.getFullYear();
       const hours = date.getHours().toString().padStart(2, "0");
       const minutes = date.getMinutes().toString().padStart(2, "0");
-      const seconds = date.getSeconds().toString();
+      const seconds = date.getSeconds().toString().padStart(2, "0");
       return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
     },
 
