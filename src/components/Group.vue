c364e71e8d260e62be7e707ff5db1149fd8b806f
lunaticaq <Lindasalan05@gmail.com>
2024-08-02 08:13:52 +0200

    css group

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,192 +1,177 @@
 <template>
-    <!-- Display group content when not editing -->
-    <div
-      v-if="!editing && isAdmin"
-      class="group"
-      @click.stop="startEdit"
-      @mouseover="showEditIcon = true"
-      @mouseleave="showEditIcon = false"
-    >
-      <!-- Group Content -->
-      <div class="group-content">
-        <h2 v-if="newTitle">{{ newTitle }}</h2>
-        <h3 v-else class="placeholder">Title</h3>
-      </div>
+  <div v-if="!editing && isAdmin" class="group" @click.stop="startEdit" @mouseover="showEditIcon = true" @mouseleave="showEditIcon = false">
+    <div class="group-content">
+      <h2 v-if="newTitle">{{ newTitle }}</h2>
+      <h3 v-else class="placeholder">Title</h3>
     </div>
-  
-    <!-- Modal for editing -->
-    <div v-else class="modal" @click="handleClickOutside">
-      <div class="modal-content">
-        <!-- Input for editing group title -->
-        <input
-          v-model="newTitle"
-          placeholder="Title"
-          class="edit-title"
-          :id="generateUniqueId('title-input')"
-          :maxlength="maxTitleLength"
-        />
-        <ul>
-          <!-- List items for editing members -->
-          <li v-for="(member, idx) in newMembers" :key="idx">
-            <div
-              @mouseover="hoverIndex = idx"
-              @mouseleave="hoverIndex = null"
-              class="item-container"
-            >
-              <!-- Input for editing member text -->
-              <input
-                v-model="newMembers[idx].text"
-                class="edit-textarea"
-                :id="generateUniqueId('item-input', idx)"
-                placeholder="Enter member here"
-                ref="itemInputs"
-                @blur="removeEmptyMember(idx)"
-              />
-              <!-- Button to remove member -->
-              <button
-                v-if="hoverIndex === idx"
-                @click.stop="removeMember(idx)"
-                class="remove-btn"
-              >
-                <i class="fa-solid fa-xmark"></i>
-              </button>
-            </div>
-          </li>
-        </ul>
-        <!-- Button to add new member -->
-        <button @click="addMember" class="add-btn">
-          <i class="fa-solid fa-plus"></i>
-        </button>
-        <!-- Edit actions buttons -->
-        <div class="edit-actions">
-          <button class="delete-btn-modal" @click.stop="deleteGroup">
-            <img src="../assets/delete.svg" alt="Delete" />
-          </button>
-          <button @click.stop="cancelEdit" class="cancel-btn">
-            <img src="../assets/X_icon.svg" alt="Cancel" />
-          </button>
-          <button @click.stop="saveEdit" class="save-btn">
-            <img src="../assets/save.svg" alt="Save" />
-          </button>
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import { loadGroups, saveGroups, updateGroups } from '../api/apiService';
-  
-  export default {
-    props: {
-      groupId: {
-        type: [String, Number],
-        required: true
-      },
-      title: {
-        type: String,
-        required: true
-      },
-      utente: {
-        type: String,
-        required: true,
-        default: ''
-      },
-      members: {
-        type: Array,
-        required: true,
-        default: () => []
-      },
-      timestamp: {
-        type: [String, Number],
-        default: new Date().toISOString()
-      },
-      type: {
-        type: String,
-        default: 'Default Type'
-      }
-    },
-    data() {
-      return {
-        newTitle: this.title,
-        newMembers: this.members.map(member => ({ text: member.text || '' })),
-        editing: false,
-        showEditIcon: false,
-        hoverIndex: null,
-        maxTitleLength: 50 // Set a default value for maxTitleLength
-      };
-    },
-    computed: {
-      formattedTimestamp() {
-        const date = new Date(this.timestamp);
-        return date.toLocaleString(); // Format as needed
-      }
-    },
-    methods: {
-        isAdmin(){
-            this.$emit("admin")
-        },
-      async saveEdit() {
-        const filteredMembers = this.newMembers.filter(member => member.text.trim() !== '');
-        const editedGroup = {
-          id: this.groupId,
-          title: this.newTitle,
-          members: filteredMembers,
-          timestamp: this.timestamp,
-          type: this.type
-        };
+  </div>
 
-        try {
-          await updateGroups(this.groupId, editedGroup);
-          this.editing = false;
-          this.showEditIcon = false;
-          this.$emit('refresh');
-        } catch (error) {
-          console.error("Failed to save group", error);
-        }
-      },
-      async deleteGroup() {
-        try {
-          const { groups } = await loadGroups();
-          const updatedGroups = groups.filter(group => group.id !== this.groupId);
-          await saveGroups(updatedGroups);
-          this.editing = false;
-          this.$emit('refresh');
-        } catch (error) {
-          console.error("Failed to delete group:", error);
-        }
-      },
-      cancelEdit() {
-        this.newTitle = this.title;
-        this.newMembers = this.members.map(member => ({ text: member.text || '' }));
+  <div v-else class="modal" @click="handleClickOutside">
+    <div class="modal-content">
+      <input
+        v-model="newTitle"
+        placeholder="Title"
+        class="edit-title"
+        :id="generateUniqueId('title-input')"
+        :maxlength="maxTitleLength"
+      />
+      <ul>
+        <li v-for="(member, idx) in newMembers" :key="idx">
+          <div
+            @mouseover="hoverIndex = idx"
+            @mouseleave="hoverIndex = null"
+            class="item-container"
+          >
+            <input
+              v-model="newMembers[idx].text"
+              class="edit-textarea"
+              :id="generateUniqueId('item-input', idx)"
+              placeholder="Enter member here"
+              ref="itemInputs"
+              @blur="removeEmptyMember(idx)"
+            />
+            <button
+              v-if="hoverIndex === idx"
+              @click.stop="removeMember(idx)"
+              class="remove-btn"
+            >
+              <i class="fa-solid fa-xmark"></i>
+            </button>
+          </div>
+        </li>
+      </ul>
+      <button @click="addMember" class="add-btn">
+        <i class="fa-solid fa-plus"></i>
+      </button>
+      <div class="edit-actions">
+        <button class="delete-btn-modal" @click.stop="deleteGroup">
+          <img src="../assets/delete.svg" alt="Delete" />
+        </button>
+        <button @click.stop="cancelEdit" class="cancel-btn">
+          <img src="../assets/X_icon.svg" alt="Cancel" />
+        </button>
+        <button @click.stop="saveEdit" class="save-btn">
+          <img src="../assets/save.svg" alt="Save" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { loadGroups, saveGroups, updateGroups } from '../api/apiService';
+
+export default {
+  props: {
+    groupId: {
+      type: [String, Number],
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    utente: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    members: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
+    timestamp: {
+      type: [String, Number],
+      default: new Date().toISOString()
+    },
+    type: {
+      type: String,
+      default: 'Default Type'
+    }
+  },
+  data() {
+    return {
+      newTitle: this.title,
+      newMembers: this.members.map(member => ({ text: member.text || '' })),
+      editing: false,
+      showEditIcon: false,
+      hoverIndex: null,
+      maxTitleLength: 50
+    };
+  },
+  computed: {
+    formattedTimestamp() {
+      const date = new Date(this.timestamp);
+      return date.toLocaleString();
+    }
+  },
+  methods: {
+    isAdmin(){
+      this.$emit("admin")
+    },
+    async saveEdit() {
+      const filteredMembers = this.newMembers.filter(member => member.text.trim() !== '');
+      const editedGroup = {
+        id: this.groupId,
+        title: this.newTitle,
+        members: filteredMembers,
+        timestamp: this.timestamp,
+        type: this.type
+      };
+
+      try {
+        await updateGroups(this.groupId, editedGroup);
         this.editing = false;
         this.showEditIcon = false;
         this.$emit('refresh');
-      },
-      generateUniqueId(prefix, index = "") {
-        return `${prefix}-${this._uid}-${index}`;
-      },
-      startEdit() {
-        this.editing = true;
-      },
-      addMember() {
-        this.newMembers.push({ text: '' });
-      },
-      removeEmptyMember() {
-        this.newMembers = this.newMembers.filter(member => member.text.trim() !== '');
-      },
-      removeMember(idx) {
-        this.newMembers.splice(idx, 1);
-      },
-      handleClickOutside(event) {
-        if (!event.target.closest(".modal-content")) {
-          this.cancelEdit();
-        }
-      },
-    }
+      } catch (error) {
+        console.error("Failed to save group", error);
+      }
+    },
+    async deleteGroup() {
+      try {
+        const { groups } = await loadGroups();
+        const updatedGroups = groups.filter(group => group.id !== this.groupId);
+        await saveGroups(updatedGroups);
+        this.editing = false;
+        this.$emit('refresh');
+      } catch (error) {
+        console.error("Failed to delete group:", error);
+      }
+    },
+    cancelEdit() {
+      this.newTitle = this.title;
+      this.newMembers = this.members.map(member => ({ text: member.text || '' }));
+      this.editing = false;
+      this.showEditIcon = false;
+      this.$emit('refresh');
+    },
+    generateUniqueId(prefix, index = "") {
+      return `${prefix}-${this._uid}-${index}`;
+    },
+    startEdit() {
+      this.editing = true;
+    },
+    addMember() {
+      this.newMembers.push({ text: '' });
+    },
+    removeEmptyMember() {
+      this.newMembers = this.newMembers.filter(member => member.text.trim() !== '');
+    },
+    removeMember(idx) {
+      this.newMembers.splice(idx, 1);
+    },
+    handleClickOutside(event) {
+      if (!event.target.closest(".modal-content")) {
+        this.cancelEdit();
+      }
+    },
+  }
 }
-  </script>
+</script>
+
 <style scoped>
-/* General Styles */
 .group, .group-content, .modal-content, .edit-title, .edit-textarea {
   background-color: var(--note-background-color);
   color: var(--note-text-color);
@@ -195,11 +180,10 @@
 .group {
   padding: 20px;
   width: 100%;
-  max-width: 400px; /* Adjust as needed */
+  max-width: 400px;
   margin-bottom: 20px;
   cursor: grab;
   position: relative;
-  /* Ensure the group is not overlapped by other elements */
   z-index: 1;
 }
 
@@ -207,7 +191,6 @@
   padding: 20px;
   border-radius: 8px;
   position: relative;
-  /* Ensure the content has proper spacing */
 }
 
 .group-content h2 {
@@ -234,7 +217,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 1000; /* Ensure the modal is on top of other elements */
+  z-index: 1000;
 }
 
 .modal-content {
@@ -245,17 +228,66 @@
   position: relative;
   background-color: var(--note-background-color);
   color: var(--note-text-color);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
-.close-btn, .cancel-btn {
+.close-btn {
   position: absolute;
   top: 5px;
   right: 5px;
+  background-color: var(--note-background-color);
+  border: none;
+  color: #4caf50;
+  font-size: 24px;
+  cursor: pointer;
+  line-height: 1;
+}
+
+ul {
+  padding: 0;
+  list-style: none; /* Remove default list style */
+}
+
+li {
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding-left: 30px; /* Adjust based on icon size */
+}
+
+li::before {
+  content: '•'; /* Custom marker */
+  font-size: 20px; /* Adjust to match the size of fa-solid fa-plus */
+  position: absolute;
+  left: 12px;
+  top: 50%;
+  transform: translateY(-50%); /* Center marker vertically */
+  color: var(--note-text-color); /* Match the color to your design */
+}
+
+.add-btn {
+  display: flex;
+  align-items: center;
+  color: #4caf50;
+  background-color: var(--note-background-color);
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 4px;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+}
+
+.remove-btn {
   background: none;
   border: none;
   color: red;
-  font-size: 24px;
   cursor: pointer;
+  display: none;
+  margin-left: 10px;
 }
 
 .edit-title {
@@ -274,34 +306,26 @@
   font-size: 16px;
   padding: 8px;
   margin-right: 10px;
-  border: 1px solid var(--note-background-color); /* Ensure no visible border */
-  border-radius: 4px; /* Optional: Add border-radius for better aesthetics */
+  border: 1px solid var(--note-background-color);
+  border-radius: 4px;
   background-color: var(--note-background-color);
   color: var(--note-text-color);
 }
 
-.remove-btn {
+.cancel-btn {
   background: none;
   border: none;
-  color: red;
+  margin-left: 10px;
   cursor: pointer;
-  display: none; /* Hide remove button by default */
-}
-
-.add-btn {
-  display: flex;
-  align-items: center;
-  background-color: #4caf50;
-  color: #fff;
-  border: none;
-  padding: 10px;
-  cursor: pointer;
-  border-radius: 4px; /* Add border-radius for consistency */
+  position: absolute;
+  top: 18px;
+  right: 18px;
 }
 
 .edit-actions {
   display: flex;
   justify-content: flex-end;
+  width: 100%;
   margin-top: 10px;
 }
 
@@ -311,17 +335,15 @@
   border: none;
   margin-left: 10px;
   cursor: pointer;
+  color: var(--note-text-color);
 }
 
-
-/* Show remove button only for the active group */
 .group.active .remove-btn {
   display: block;
 }
 
-/* Optional: Add hover and focus styles for better UX */
 .add-btn:hover, .add-btn:focus {
-  background-color: #45a049;
+  background-color: var(--note-background-color);
 }
 
 .remove-btn:hover, .remove-btn:focus {
